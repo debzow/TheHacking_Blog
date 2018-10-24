b627b50c63e3336e5cd3d8b67c1515689f77fbf3
debzow <david.gerard.42@gmail.com>
2018-10-24 16:18:04 +0200

    add of table like

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-# 10.times do
-#   user = User.create!(first_name: Faker::Hobbit.character, last_name: Faker::Company.name, email: Faker::Internet.email)
-# end
+#create 10 users in the table user
+10.times do
+  user = User.create!(first_name: Faker::Hobbit.character, last_name: Faker::Company.name, email: Faker::Internet.email)
+end
 
+#create 5 categories in the table category
+5.times do
+  category = Category.create!(name: Faker::Simpsons.character, description: Faker::Simpsons.location)
+end
 
-# 5.times do
-#   category = Category.create!(name: Faker::Simpsons.character, description: Faker::Simpsons.location)
-# end
-
-
+#create 10 articles in the table article
 10.times do
   article = Article.create!(title: Faker::RickAndMorty.character, content: Faker::RickAndMorty.quote, user_id: rand(1..10), category_id: rand(1..5))
 end
 
+#create 15 comments in the table comment
 15.times do
   comment = Comment.create!(content: Faker::Hipster.sentences, user_id: rand(1..10), article_id: rand(1..10))
+end
+
+#create 15 likes in the table like
+15.times do
+  like = Like.create!(user_id: rand(1..10), article_id: rand(1..10))
 end
