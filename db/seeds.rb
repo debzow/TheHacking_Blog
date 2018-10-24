a93cd06f92ad87b2241cd27ff9e5b016b0288e4f
debzow <david.gerard.42@gmail.com>
2018-10-24 15:55:31 +0200

    initialization of rails and database

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# 10.times do
+#   user = User.create!(first_name: Faker::Hobbit.character, last_name: Faker::Company.name, email: Faker::Internet.email)
+# end
+
+
+# 5.times do
+#   category = Category.create!(name: Faker::Simpsons.character, description: Faker::Simpsons.location)
+# end
+
+
+10.times do
+  article = Article.create!(title: Faker::RickAndMorty.character, content: Faker::RickAndMorty.quote, user_id: rand(1..10), category_id: rand(1..5))
+end
+
+15.times do
+  comment = Comment.create!(content: Faker::Hipster.sentences, user_id: rand(1..10), article_id: rand(1..10))
+end
